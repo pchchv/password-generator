b8664a9c28f3d7a424ba2bafe72bbc3c1e8d08d0
pchchv <ipchchv@gmail.com>
2022-09-23 11:02:36 +0300

    Create a snippet to launch the application

---
+++  password-generator/app.py
@@ -0,0 +1,21 @@
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+from ui_main import Ui_MainWindow
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super(App, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = App()
+    window.show()
+
+    sys.exit(app.exec())
