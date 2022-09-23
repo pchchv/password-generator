6284c248aead69f1515769e9fecfe713cb5121bb
pchchv <ipchchv@gmail.com>
2022-09-23 20:31:01 +0300

    Implement password entropy retrieval

---  password-generator/password.py
+++  password-generator/password.py
@@ -1,5 +1,14 @@
+from math import log2
 import secrets
 
 
 def create_new(length: int, characters: str) -> str:
     return ''.join(secrets.choice(characters) for _ in range(length))
+
+
+def get_entropy(length: int, character_number: int) -> float:
+    try:
+        entropy = length * log2(character_number)
+    except ValueError:
+        return 0.0
+    return round(entropy, 2)
