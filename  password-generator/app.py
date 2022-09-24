5c62b3f784163c18b1445e490dfb7fe1c5b966aa
pchchv <ipchchv@gmail.com>
2022-09-24 11:50:46 +0300

    Implement a method receives symbols of the buttons

---  password-generator/app.py
+++  password-generator/app.py
@@ -1,8 +1,7 @@
 import sys
-
 from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_main import Ui_MainWindow
-
+import buttons
 
 class PasswordGenerator(QMainWindow):
     def __init__(self):
@@ -15,6 +14,13 @@ class PasswordGenerator(QMainWindow):
         self.ui.slider_length.valueChanged.connect(self.ui.spinbox_length.setValue)
         self.ui.spinbox_length.valueChanged.connect(self.ui.slider_length.setValue)
         self.ui.spinbox_length.valueChanged.connect(self.set_password)
+    
+    def get_characters(self) -> str:
+        chars = ''
+        for btn in buttons.Characters:
+            if getattr(self.ui, btn.name).isChecked():
+                chars += btn.value
+        return chars
 
 
 if __name__ == "__main__":
