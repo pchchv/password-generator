bb587d3244159df17b9a7133ee5135ad37efd0ad
pchchv <ipchchv@gmail.com>
2022-09-24 11:45:56 +0300

    Rename class

---  password-generator/app.py
+++  password-generator/app.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow
 from ui_main import Ui_MainWindow
 
 
-class App(QMainWindow):
+class PasswordGenerator(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
         self.ui = Ui_MainWindow()
@@ -14,7 +14,7 @@ class App(QMainWindow):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = App()
+    window = PasswordGenerator()
     window.show()
 
     sys.exit(app.exec())
